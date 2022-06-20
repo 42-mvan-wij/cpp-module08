3496bbf677791ba0f09cf53f6befbaf47c4c5681
Mariusmivw <mvan-wij@student.codam.nl>
2022-06-20 13:24:24 +0200

    add ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "MutantStack.hpp"
+#include <vector>
+
+__attribute__((destructor))
+void check_leaks() {
+	std::cout << std::endl;
+	system("leaks -q mutant");
+}
+
+int main()
+{
+	{
+		std::cout << "MutantStack:" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+
+	std::cout << std::endl;
+
+	{
+		std::cout << "std::vector:" << std::endl;
+		std::vector<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		//[...]
+		mstack.push_back(0);
+		std::vector<int>::iterator it = mstack.begin();
+		std::vector<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	return (EXIT_SUCCESS);
+}
